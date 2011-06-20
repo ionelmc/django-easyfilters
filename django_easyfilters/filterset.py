7d15c09eec73bcd36a1c1d8163b183aee6eacbdf
Luke Plant <L.Plant.98@cantab.net>
2011-06-20 02:57:56 +0100

    Some actual useful implementation - filtering and links kind of works for ForeignKey

--- django_easyfilters/filterset.py
+++ django_easyfilters/filterset.py
@@ -1,41 +1,135 @@
+from collections import namedtuple
+
+from django.db import models
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
+from django.utils.http import urlencode
 
 standard_filter_mapping = {
-
 }
 
-class Filter(object):
 
-    def get_choices(self, qs, params, request=None):
+FilterChoice = namedtuple('FilterChoice', 'label count url')
+
+
+class FilterOptions(object):
+    """
+    Defines some common options for all Filters.
+
+    A FilterOption instance can be used when defining the 'fields' attribute of
+    a FilterSet. The actual choice of Filter subclass will be done by the
+    FilterSet in this case.
+    """
+    def __init__(self, field, query_param=None):
+        # State: Filter objects are created as class attributes of FilterSets,
+        # and so cannot carry any request specific state. They only have
+        # configuration information.
+        self.field = field
+        self.query_param = query_param
+
+
+class Filter(FilterOptions):
+    """
+    A Filter creates links/URLs that correspond to some DB filtering,
+    and can apply the information from a URL to filter a QuerySet.
+    """
+    def apply_filter(self, qs, params):
+        p_val = params.get(self.query_param, None)
+        if p_val is None:
+            return qs
+        else:
+            return qs.filter(**{self.field: p_val})
+
+    def build_query_string(self, params, filter_val):
+        params = params.copy()
+        params[self.query_param] = filter_val
+        params.pop('page', None) # links should reset paging
+        return '?%s' % urlencode(params)
+
+    def get_choices(self, qs, params):
+        """
+        Returns a list of namedtuples containing
+        (label (as a string), count, url)
+        """
+        field_obj = qs.model._meta.get_field(self.field)
+
+        # First get the IDs and counts in one query.
+        ids_counts = qs.values_list(self.field).order_by(self.field).annotate(models.Count(self.field))
+
+        # Then get the instances, so that we can get the unicode() of them,
+        # using their normal manager to get them in normal order.
+        rel_model = field_obj.rel.to
+        rel_field = field_obj.rel.get_related_field()
+        count_dict = {}
+        for id, count in ids_counts:
+            count_dict[id] = count
+
+        # Filter to the ones relevant to our queryset:
+        lookup = {rel_field.attname + '__in': [x[0] for x in ids_counts]}
+        objs = rel_model.objects.filter(**lookup)
+        choices = []
+
+        for o in objs:
+            id = getattr(o, rel_field.attname)
+            choices.append(FilterChoice(unicode(o),
+                                        count_dict[id],
+                                        self.build_query_string(params, id)))
+        return choices
+
+    def get_remove_url(self, params, request=None):
+        """
+        Returns a URL for removing the filter, or None
+        if the filter is not currently in use.
+        """
         pass
 
-    def get_remove_link(self, params, request=None):
-        pass
+    # TODO - move all this HTML specific stuff to FilterSet
+    def render(self, qs, params):
+        out = []
+        field_obj = qs.model._meta.get_field(self.field)
+        label = field_obj.verbose_name
+        for c in self.get_choices(qs, params):
+            out.append(u'<a href="%s">%s</a> (%d) &nbsp;&nbsp;' % (escape(c.url), escape(c.label), c.count))
+        return u'<div>%s: %s</div>' % (escape(label), u''.join(out))
+
 
 class FilterSet(object):
 
-    filter_mapping = standard_filter_mapping
-    include_count = True
-
-
-    def __init__(self, params, queryset, request=None):
+    def __init__(self, queryset, params, request=None):
         self.params = params
         self.initial_queryset = queryset
-        self.qs = self.apply_filters(params, queryset)
+        self.model = queryset.model
+        self.filters = self.setup_filters()
+        self.qs = self.apply_filters(queryset, params)
 
-    def apply_filters(self, params, queryset):
+    def apply_filters(self, queryset, params):
+        for f in self.filters:
+            queryset = f.apply_filter(queryset, params)
         return queryset
 
     def render(self):
-        pass
+        return mark_safe(u'\n'.join(f.render(self.qs, self.params) for f in self.filters))
 
     def get_fields(self):
         return self.fields
 
-    def get_filters(self):
+    def get_filter_for_field(self, field, **kwargs):
+        return Filter(field, **kwargs)
+
+    def setup_filters(self):
+        # This could run once, as soon as FilterSet is created
         filters = []
-        for f in fields:
-            if isinstance(basestring, f):
-                filters.append(get_filter_for_field(f))
-            else:
-                filters.append(f)
+        for i, f in enumerate(self.get_fields()):
+            if isinstance(f, basestring):
+                f = self.get_filter_for_field(f)
+            elif isinstance(f, FilterOptions):
+                opts = f.__dict__.copy()
+                field = opts.pop('field')
+                f = self.get_filter_for_field(field, **opts)
+            if f.query_param is None:
+                f.query_param = 'f%d' % (i+1)
+            filters.append(f)
         return filters
+
+    def __unicode__(self):
+        return self.render()
--- django_easyfilters/tests/filterset.py
+++ django_easyfilters/tests/filterset.py
@@ -6,19 +6,51 @@ from django_easyfilters import FilterSet
 from models import Book, Genre
 
 
-class BookFilterSet(FilterSet):
-    fields = [
-        'binding',
-        ]
-    model = Book
-
-
 class TestFilterSet(TestCase):
 
     fixtures = ['django_easyfilters_tests']
 
-    def test_no_filters(self):
+    def test_queryset_no_filters(self):
+        class BookFilterSet(FilterSet):
+            fields = []
+            model = Book
+
         qs = Book.objects.all()
         data = {}
-        f = BookFilterSet(data, qs)
+        f = BookFilterSet(qs, data)
         self.assertEqual(qs.count(), f.qs.count())
+
+    def test_foreignkey_filters_produced(self):
+        """
+        A ForeignKey should produce a list of the possible related objects,
+        with counts.
+        """
+        class BookFilterSet(FilterSet):
+            fields = [
+                'genre',
+                ]
+            model = Book
+
+        # Make another Genre that isn't used
+        new_g, created = Genre.objects.get_or_create(name='Nonsense')
+        assert created
+
+        qs = Book.objects.all()
+        data = {}
+        fs = BookFilterSet(qs, data)
+
+        choices = [(c.label, c.count) for c in fs.filters[0].get_choices(qs, data)]
+
+        reached = [False, False]
+        for g in Genre.objects.all():
+            count = g.book_set.count()
+            if count == 0:
+                reached[0] = True
+                self.assertTrue((g.name, count) not in choices)
+            else:
+                reached[1] = True
+                self.assertTrue((g.name, count) in choices)
+
+        self.assertTrue(reached[0])
+        self.assertTrue(reached[1])
+
