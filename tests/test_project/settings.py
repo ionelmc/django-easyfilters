60290b0ae96f144cc3b5672a47596355fe117ba7
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-10-12 16:43:39 +0300

    Remove tz context processor (not available in 1.3)

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -22,7 +22,6 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
-    "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
 ]
